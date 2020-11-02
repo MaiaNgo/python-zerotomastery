6a25e67a3e48f9e298ff08f3f218a0777abb45b8
Maia Ngo <maia.groundwatercapital@gmail.com>
2020-11-02 10:59:28 -0500

    boolean & list_methods

---
+++ boolean.py
@@ -0,0 +1,13 @@
+#booleans
+name = 'Paul Mitchell'
+age = 45
+relationship_status = 'single'
+relationship_status = 'it\'s complicated'
+print(relationship_status)
+
+birth_year = input('What year were you born?')
+
+
+age = 2020 - int(birth_year)
+
+print(f'Your age is :{age} ')
